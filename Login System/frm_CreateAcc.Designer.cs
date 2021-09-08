688dd6c9a89c3f8da64d3a926a67723432658fc3
feirie001@gmail.com <feirie001@gmail.com>
2021-09-08 23:56:27 +0100

    changed font

--- Login System/frm_CreateAcc.Designer.cs
+++ Login System/frm_CreateAcc.Designer.cs
@@ -35,10 +35,11 @@ namespace Login_System
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
             this.label1.Location = new System.Drawing.Point(72, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.Size = new System.Drawing.Size(155, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Get Started";
             // 
