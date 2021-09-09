cd7d1792fba27f7e467aa465b99a55ad5f2d4d71
feirie001@gmail.com <feirie001@gmail.com>
2021-09-09 22:13:19 +0100

    minor changes

--- Login System/Form1.Designer.cs
+++ Login System/Form1.Designer.cs
@@ -75,6 +75,7 @@ namespace Login_System
             this.button1.TabIndex = 25;
             this.button1.Text = "REGISTER";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkbxShowPass
             // 
--- Login System/Form1.cs
+++ Login System/Form1.cs
@@ -16,10 +16,34 @@ namespace Login_System
         public Frm_Register()
         {
             InitializeComponent();
+        }
+        OleDbConnection con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = db_User.mdb");
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter adap = new OleDbDataAdapter();
 
-            OleDbConnection Con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = db_User.mdb"); 
-            OleDbCommand cmd = new OleDbCommand();
-            OleDbDataAdapter adap = new OleDbDataAdapter();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" && txtPassword.Text == "" && txtComPassword.Text == "")
+            {
+                MessageBox.Show("Username and Password Fiels are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == txtComPassword.Text)
+            {
+                con.Open();
+                String Register = "INSERT INTO tbl_User Values {" + txtUsername.Text + " , " + txtPassword.Text + ")";
+                cmd = new OleDbCommand(Register, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Your Account has Successfully Been Created", "Registration Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Password does not match, Please Re-enter", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Text = "";
+                txtComPassword.Text = "";
+                txtPassword.Focus();
+            }
         }
     }
 }
