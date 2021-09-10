3b9fbed52b021bd008a4177113f42f6f18983628
feirie001@gmail.com <feirie001@gmail.com>
2021-09-10 13:02:13 +0100

    email connection

--- Login System/Form1.cs
+++ Login System/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Net.Mail;
+using System.Net;
 
 namespace Login_System
 {
@@ -17,37 +19,70 @@ namespace Login_System
         {
             InitializeComponent();
         }
-        OleDbConnection con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = db_User.mdb");
-        OleDbCommand cmd = new OleDbCommand();
-        OleDbDataAdapter adap = new OleDbDataAdapter();
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txtEmail.Text == "" && txtPassword.Text == "" && txtComPassword.Text == "")
-            {
-                MessageBox.Show("Username and Password Fiels are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (txtPassword.Text == txtComPassword.Text)
-            {
-                con.Open();
-                String Register = "INSERT INTO tbl_User Values {" + txtEmail.Text + " , " + txtPassword.Text + ")";
-                cmd = new OleDbCommand(Register, con);
-                OleDbDataReader dbDataReader = cmd.ExecuteReader();
-                con.Close();
+            string to, from, pass, messageBody;
+            MailMessage message = new MailMessage();
+            to = txtEmail.Text;
+            from = "janeaustinasia@gmail.com";
+            pass = "Favzy1999";
+            messageBody = txtEmail.Text;
+            message.To.Add(to);
+            message.From = new MailAddress(from);
+            message.Body = "From : " + " Message : " + messageBody;
+            message.Subject = txtEmail.Text;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential(from, pass);
 
-                txtEmail.Text = "";
-                txtPassword.Text = "";
-                txtComPassword.Text = "";
-
-                MessageBox.Show("Your Account has Successfully Been Created", "Registration Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
+            try
             {
-                MessageBox.Show("Password does not match, Please Re-enter", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                txtPassword.Text = "";
-                txtComPassword.Text = "";
-                txtPassword.Focus();
+                client.Send(message);
+                DialogResult code = MessageBox.Show("Invalid Username or Password, Please Try Again", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                if (code == DialogResult.OK)
+                {
+                    txtEmail.Clear();
+                    txtPassword.Clear();
+                    new DashBoard().Show();
+                    this.Show();
+                }
+
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //if (txtEmail.Text == "" && txtPassword.Text == "" && txtComPassword.Text == "")
+            //{
+            //    MessageBox.Show("Username and Password Fiels are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+            //else if (txtPassword.Text == txtComPassword.Text)
+            //{
+            //    con.Open();
+            //    String Register = "INSERT INTO tbl_User Values {" + txtEmail.Text + " , " + txtPassword.Text + ")";
+            //    cmd = new OleDbCommand(Register, con);
+            //    OleDbDataReader dbDataReader = cmd.ExecuteReader();
+            //    con.Close();
+
+            //    txtEmail.Text = "";
+            //    txtPassword.Text = "";
+            //    txtComPassword.Text = "";
+
+            //    MessageBox.Show("Your Account has Successfully Been Created", "Registration Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Password does not match, Please Re-enter", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    txtPassword.Text = "";
+            //    txtComPassword.Text = "";
+            //    txtPassword.Focus();
+            //}
         }
 
         private void checkbxShowPass_CheckedChanged(object sender, EventArgs e)
@@ -88,13 +123,22 @@ namespace Login_System
 
         private void txtmail_Enter(object sender, EventArgs e)
         {
-            
+            if (txtEmail.Text == "  Email")
+            {
+                txtEmail.Clear();
+                txtEmail.ForeColor = Color.FromArgb(83, 179, 233);
+
+            }
 
         }
 
         private void txtmail_Leave(object sender, EventArgs e)
         {
-           
+            if (txtEmail.Text == "")
+            {
+                txtEmail.ForeColor = Color.FromArgb(200, 200, 200);
+                txtEmail.Text = "   Email";
+            }
         }
 //        con.Open();
 //            string to, from, pass, messageBody;
