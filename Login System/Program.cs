1382eda68d5e74389c9e6a531a26a7c39898d3b0
feirie001@gmail.com <feirie001@gmail.com>
2021-09-18 12:50:34 +0100

    change application startup

--- Login System/Program.cs
+++ Login System/Program.cs
@@ -16,7 +16,7 @@ namespace Login_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_Register());
+            Application.Run(new frm_Login());
         }
     }
 }
